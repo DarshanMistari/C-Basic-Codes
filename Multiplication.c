0b7633b4d5854b516f3c971bc81008c42a56d49e
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-23 23:17:45 +0530

    [ADDED]:Darshan - Multiplication of Two Number

---
+++ Multiplication.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int mul;
+	int num1=5;
+	int num2=4;
+	
+	printf("Enter the Two Number :");
+	scanf("%d %d",&num1,&num2);
+	
+	mul=num1*num2;
+	printf("Multiplication :%d",mul);
+}
