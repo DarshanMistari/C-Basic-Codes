fe1a61c0e0e4b6b6319297602414ba5e04185e07
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-26 07:08:58 +0530

    [ADDED]:Darshan - String Concat

---
+++ String Concat.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char ch[10]={'H','e','l','l','o'};
+	char ch2[10]="Darshan";
+	strcat(ch,ch2);
+	printf("Value of first string is : %s\n",ch);
+	return 0;
+	
+}
+
