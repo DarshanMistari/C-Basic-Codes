4ea228d17b26691695b3d1a1bc5d0f2b5dc83142
DarshanMistari <mistaridarshan2@gmail.com>
2024-04-08 23:38:55 +0530

    [ADDED]:Darshan

---
+++ Swastik.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	printf("\n* \t**********");
+	printf("\n* \t*");
+	printf("\n* \t*");
+	printf("\n* \t*");
+	printf("\n* \t*");
+	printf("\n*****************");
+	printf("\n \t* \t*");
+	printf("\n \t* \t*");
+	printf("\n \t* \t*");
+	printf("\n \t* \t*");
+	printf("\n*********\t*");
+	
+	return 0;
+}
