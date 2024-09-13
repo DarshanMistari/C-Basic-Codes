181c8b24fccd217b9c210658a583afb7b59032ee
DarshanMistari <mistaridarshan2@gmail.com>
2024-09-13 20:08:34 +0530

    [ADDED]:Darshan c code H Pattern

---
+++ H_Pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	for(int i=1; i<=7; i++)
+    {
+        for(int j=1; j<=7; j++)
+        {
+            if(j==1 || i==4 || j==7)
+            {
+                printf("+");
+            }
+            else{
+            printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
