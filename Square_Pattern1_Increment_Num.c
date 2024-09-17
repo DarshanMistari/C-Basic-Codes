843831154227d4fe4c42d06eac330c0dab455647
DarshanMistari <mistaridarshan2@gmail.com>
2024-09-17 21:32:01 +0530

    [ADDED]:Darshan c code Square  increment num Pattern

---
+++ Square_Pattern1_Increment_Num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int row=5;
+    int col=5;
+    int i;
+    int j;
+    
+    for(i=1 ; i<=row ; i++)
+	{
+		for ( j = 1; j <= col; j++)
+		{
+		   printf(i);
+        }
+		printf("\n");
+	}
+}
